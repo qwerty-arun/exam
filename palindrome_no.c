395b7d2a18b1ac39ee039809250b51209a086b3f
qwerty-arun <114285027+qwerty-arun@users.noreply.github.com>
2023-05-27 22:30:41 +0530

    Update palindrome_no.c

--- palindrome_no.c
+++ palindrome_no.c
@@ -1,1 +1,29 @@
-
+# include <stdio.h>
+int reverse(int n);
+int rev=0,digits=0;
+int main()
+{
+  int n;
+  printf("Enter a no.\n");
+  scanf("%d",&n);
+  int k=reverse(n);
+  if(k==n)
+  {
+    printf("The no. you entered is palindrome\n");
+  }
+  else
+  {
+    printf("The no. you enter is NOT palindrome\n");
+  }
+}
+int reverse(int n)
+{
+  while(n>0)
+  {
+    int r=n%10;
+    rev=rev*10+r;
+    digits++;
+    n=n/10;
+  }
+return rev;
+}
