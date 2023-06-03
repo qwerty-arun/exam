01639036db157502ac5b28a749c7124873ee3a1f
qwerty-arun <114285027+qwerty-arun@users.noreply.github.com>
2023-06-03 09:21:47 +0530

    Create swapping_using_pointers.c

---
+++ swapping_using_pointers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void swap(int* , int *);
+int main() {
+   int a=6,b=8;
+   printf("Before swapping: a=%d and b=%d",a,b);
+   swap(&a,&b);
+    return 0;
+}
+void swap(int* p,int *q)
+{
+    *p=(*p) + (*q);
+    *q=(*p) - (*q);
+    *p=(*p) - (*q);
+    printf("After swapping: a=%d and b=%d",*p,*q); 
+}
